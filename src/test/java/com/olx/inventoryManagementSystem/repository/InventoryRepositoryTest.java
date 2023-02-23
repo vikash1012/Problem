b127dda4c5efc71ec04907bf0b489259f62077e8
Gollapalli Likhita <gollapalli.likhita@olx.com>
2023-02-23 19:08:20 +0530

    add tests to InventoryRepository

--- src/test/java/com/olx/inventoryManagementSystem/repository/InventoryRepositoryTest.java
+++ src/test/java/com/olx/inventoryManagementSystem/repository/InventoryRepositoryTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.olx.inventoryManagementSystem.controller.dto.SecondaryStatus;
+import com.olx.inventoryManagementSystem.exceptions.InvalidTypeException;
 import com.olx.inventoryManagementSystem.exceptions.InventoryNotFoundException;
 import com.olx.inventoryManagementSystem.model.Inventory;
 import org.junit.jupiter.api.Assertions;
@@ -12,12 +13,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -68,7 +71,7 @@ class InventoryRepositoryTest {
     }
 
     @Test
-    void ShouldReturnInventoryForInValidSku() throws InventoryNotFoundException {
+    void ShouldReturnExceptionForInValidSku() throws InventoryNotFoundException {
         String sku = "d59fdbd5-0c56-4a79-8905-6989601890be";
         Optional<Inventory> inventory=Optional.empty();
         when(jpaInventoryRepository.findById(sku)).thenReturn(inventory);
@@ -77,29 +80,56 @@ class InventoryRepositoryTest {
         InventoryNotFoundException actualError= Assertions.assertThrows(InventoryNotFoundException.class,()->inventoryRepository.findInventory(sku));
 
         assertEquals(expectedError,actualError.getMessage());
-
-    }
-
-//    @Test
-//    void shouldReturnfetchInventories() {
-//        String sku = "d59fdbd5-0c56-4a79-8905-6989601890bf";
-//        JsonNode attributes = new ObjectMapper().createObjectNode();
-//        ((ObjectNode) attributes).put("vin", "AP31CM9873");
-//        ((ObjectNode) attributes).put("make", "Tata");
-//        ((ObjectNode) attributes).put("model", "Nexon");
-//        ArrayList<SecondaryStatus> secondaryStatus = new ArrayList<>();
-//        secondaryStatus.add(new SecondaryStatus("warehouse", "in-repair"));
-//        Inventory inventory = new Inventory("d59fdbd5-0c56-4a79-8905-6989601890bf", "car", "Mumbai", LocalDateTime.of(2023, 2, 21, 22, 59), LocalDateTime.of(2023, 2, 21, 22, 59), "user", "user", attributes, 450000, secondaryStatus);
-//
-//        doNothing().doThrow(new RuntimeException()).when(jpaInventoryRepository).findById(sku);
-//        verify(jpaInventoryRepository, times(1)).findById(sku);
-//    }
-
-    @Test
-    void updateStatus() {
     }
 
     @Test
-    void addStatus() {
+    void shouldReturnPageOfInventories() {
+        JsonNode attributes = new ObjectMapper().createObjectNode();
+        ((ObjectNode) attributes).put("vin", "AP31CM9873");
+        ((ObjectNode) attributes).put("make", "Tata");
+        ((ObjectNode) attributes).put("model", "Nexon");
+        ArrayList<SecondaryStatus> secondaryStatus = new ArrayList<>();
+        secondaryStatus.add(new SecondaryStatus("warehouse", "in-repair"));
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Order.asc("sku")));
+        Page<Inventory> expectedInventories = new PageImpl<>(List.of(new Inventory("d59fdbd5-0c56-4a79-8905-6989601890be", "car", "Mumbai", LocalDateTime.of(2023, 2, 21, 22, 59), LocalDateTime.of(2023, 2, 21, 22, 59), "user", "user", attributes, 450000, secondaryStatus)));
+        when(jpaInventoryRepository.findAll(pageable)).thenReturn(expectedInventories);
+
+        Page<Inventory> actualInventories = inventoryRepository.fetchInventories(pageable);
+
+        assertEquals(expectedInventories,actualInventories);
+    }
+
+    @Test
+    void ShouldReturnSkuAndUpdateStatus() {
+        String expectedSku = "d59fdbd5-0c56-4a79-8905-6989601890bf";
+        JsonNode attributes = new ObjectMapper().createObjectNode();
+        ((ObjectNode) attributes).put("vin", "AP31CM9873");
+        ((ObjectNode) attributes).put("make", "Tata");
+        ((ObjectNode) attributes).put("model", "Nexon");
+        ArrayList<SecondaryStatus> secondaryStatus = new ArrayList<>();
+        secondaryStatus.add(new SecondaryStatus("warehouse", "in-repair"));
+        Inventory inventory = new Inventory("d59fdbd5-0c56-4a79-8905-6989601890bf", "car", "Mumbai", LocalDateTime.of(2023, 2, 21, 22, 59), LocalDateTime.of(2023, 2, 21, 22, 59), "user", "user", attributes, 450000, secondaryStatus);
+        when(jpaInventoryRepository.findById("d59fdbd5-0c56-4a79-8905-6989601890bf")).thenReturn(Optional.of(inventory));
+
+        String actualSku = inventoryRepository.updateStatus("d59fdbd5-0c56-4a79-8905-6989601890bf", new SecondaryStatus("warehouse","working"));
+
+        assertEquals(expectedSku,actualSku);
+    }
+
+    @Test
+    void ShouldReturnStatusAndUpdateStatus() {
+        String expectedSku = "d59fdbd5-0c56-4a79-8905-6989601890bf";
+        JsonNode attributes = new ObjectMapper().createObjectNode();
+        ((ObjectNode) attributes).put("vin", "AP31CM9873");
+        ((ObjectNode) attributes).put("make", "Tata");
+        ((ObjectNode) attributes).put("model", "Nexon");
+        ArrayList<SecondaryStatus> secondaryStatus = new ArrayList<>();
+        secondaryStatus.add(new SecondaryStatus("warehouse", "in-repair"));
+        Inventory inventory = new Inventory("d59fdbd5-0c56-4a79-8905-6989601890bf", "car", "Mumbai", LocalDateTime.of(2023, 2, 21, 22, 59), LocalDateTime.of(2023, 2, 21, 22, 59), "user", "user", attributes, 450000, secondaryStatus);
+        when(jpaInventoryRepository.findById("d59fdbd5-0c56-4a79-8905-6989601890bf")).thenReturn(Optional.of(inventory));
+
+        String actualSku = inventoryRepository.addStatus("d59fdbd5-0c56-4a79-8905-6989601890bf",new SecondaryStatus("legal","in-progress"));
+
+        assertEquals(expectedSku,actualSku);
     }
 }
