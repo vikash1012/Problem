82b8a977d72880ba7819d9cc1053064fe0debe35
Gollapalli Likhita <gollapalli.likhita@olx.com>
2023-03-07 13:01:26 +0530

    modify userServiceTest and loginUserService Test

--- src/test/java/com/olx/inventoryManagementSystem/service/UserServiceTest.java
+++ src/test/java/com/olx/inventoryManagementSystem/service/UserServiceTest.java
@@ -1,74 +1,67 @@
-//package com.olx.inventoryManagementSystem.service;
-//
-//import com.olx.inventoryManagementSystem.controller.dto.RegistrationResponse;
-//import com.olx.inventoryManagementSystem.controller.dto.UserRequest;
-//import com.olx.inventoryManagementSystem.model.User;
-//import com.olx.inventoryManagementSystem.repository.UserRepository;
-//import com.olx.inventoryManagementSystem.security.WebSecurityConfig;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.when;
-//
-//@ExtendWith(MockitoExtension.class)
-//class UserServiceTest {
-//
-//    @Autowired
-//    AuthenticationManagerBuilder auth;
-//    @Mock
-//    UserRepository userRepository;
-//
-//    UserService userService;
-//
-//    @Mock
-//    WebSecurityConfig webSecurityConfig;
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return NoOpPasswordEncoder.getInstance();
-//    }
-//
-//    @BeforeEach
-//    void setup() {
-//        userService = new UserService(userRepository, webSecurityConfig);
-//    }
-//
-//    @Test
-//    public void ShouldReturnRegistrationResponseWhenUserIsAlreadyPresent() {
-//        User user = new User("parimalvarma@gmail.com", "vparimal587");
-//        when(userRepository.userExistByEmail("parimalvarma@gmail.com")).thenReturn(user);
-//        ResponseEntity<RegistrationResponse> expectedResponse = new ResponseEntity<>(new RegistrationResponse("User already exists"), HttpStatus.CONFLICT);
-//
-//        ResponseEntity<RegistrationResponse> actualResponse = userService.createUser(new UserRequest("parimalvarma@gmail.com", "vparimal587"));
-//
-//        assertEquals(expectedResponse, actualResponse);
-//    }
-//
-//    @Test
-//    public void ShouldReturnRegistrationResponseWhenUserIsCreated() {
-//        PasswordEncoder passwordEncoder = null;
-//        UserRequest userRequest = new UserRequest("parimalvarma@gmail.com", "vparimal587");
-//        when(userRepository.userExistByEmail("parimalvarma@gmail.com")).thenReturn(null);
-//        when(webSecurityConfig.passwordEncoder()).thenReturn(new BCryptPasswordEncoder());
-//        // TODO: Do not use any()
-//        when(userRepository.createUser(any())).thenReturn("User Registered Successfully");
-//        ResponseEntity<RegistrationResponse> expectedResponse = new ResponseEntity(new RegistrationResponse("User Registered Successfully"), HttpStatus.CREATED);
-//
-//        ResponseEntity<RegistrationResponse> actualResponse = userService.createUser(new UserRequest("parimalvarma@gmail.com", "vparimal587"));
-//
-//        assertEquals(expectedResponse, actualResponse);
-//    }
-//}
+package com.olx.inventoryManagementSystem.service;
+
+import com.olx.inventoryManagementSystem.controller.dto.UserRequest;
+import com.olx.inventoryManagementSystem.exceptions.UserAlreadyExistsException;
+import com.olx.inventoryManagementSystem.model.User;
+import com.olx.inventoryManagementSystem.repository.UserRepository;
+import com.olx.inventoryManagementSystem.security.WebSecurityConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    UserService userService;
+
+    @Mock
+    WebSecurityConfig webSecurityConfig;
+
+    @BeforeEach
+    void setup() {
+        userService = new UserService(userRepository, webSecurityConfig);
+    }
+
+    @Test
+    public void ShouldThrowUserAlreadyException() {
+        String email = "user@email.com";
+        User user = new User(email, "vparimal587");
+        when(userRepository.userExistByEmail(email)).thenReturn(user);
+        String expectedResponse = "User Already Exists";
+
+        Exception actualError = Assertions.assertThrows(Exception.class, () -> userService.createUser(new UserRequest(email, "vparimal587")));
+
+        assertEquals(expectedResponse, actualError.getMessage());
+        verify(userRepository, times(1)).userExistByEmail(email);
+    }
+
+    @Test
+    public void ShouldReturnEmailWhenUserIsCreated() throws UserAlreadyExistsException {
+        String email = "user@email.com";
+        when(userRepository.userExistByEmail(email)).thenReturn(null);
+        when(webSecurityConfig.passwordEncoder()).thenReturn(new BCryptPasswordEncoder());
+        when(userRepository.createUser(any(User.class))).thenReturn(email);
+        String expectedResponse = email;
+
+        String actualResponse = userService.createUser(new UserRequest(email, "vparimal587"));
+
+        assertEquals(expectedResponse, actualResponse);
+        verify(userRepository, times(1)).userExistByEmail(email);
+        verify(webSecurityConfig, times(1)).passwordEncoder();
+        verify(userRepository, times(1)).createUser(any(User.class));
+    }
+
+}
